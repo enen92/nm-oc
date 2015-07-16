43ede9cf3c240f0b304883b3b4e9b3e30a1fbc48
Lubomir Rintel <lkundrak@v3.sk>
2015-07-16 15:53:24 +0200

    properties: use the NM error types & quarks

--- properties/nm-openconnect.c
+++ properties/nm-openconnect.c
@@ -61,12 +61,18 @@
 #define OPENCONNECT_PLUGIN_NAME    _("Cisco AnyConnect Compatible VPN (openconnect)")
 #define OPENCONNECT_PLUGIN_DESC    _("Compatible with Cisco AnyConnect SSL VPN.")
 
+#define OPENCONNECT_PLUGIN_UI_ERROR                   NM_SETTING_VPN_ERROR
+#define OPENCONNECT_PLUGIN_UI_ERROR_INVALID_PROPERTY  NM_SETTING_VPN_ERROR_INVALID_PROPERTY
+
 #else /* !NM_OPENCONNECT_OLD */
 
 #include <NetworkManager.h>
 
 #define OPENCONNECT_PLUGIN_NAME    _("openconnect")
 #define OPENCONNECT_PLUGIN_DESC    _("Cisco AnyConnect SSL Compatible VPN")
+
+#define OPENCONNECT_PLUGIN_UI_ERROR                   NM_CONNECTION_ERROR
+#define OPENCONNECT_PLUGIN_UI_ERROR_INVALID_PROPERTY  NM_CONNECTION_ERROR_INVALID_PROPERTY
 #endif
 
 #include "nm-openconnect-service-defines.h"
@@ -352,40 +358,6 @@ export (NMVpnEditorPlugin *iface,
 done:
 	fclose (f);
 	return success;
-}
-
-GQuark
-openconnect_plugin_ui_error_quark (void)
-{
-	static GQuark error_quark = 0;
-
-	if (G_UNLIKELY (error_quark == 0))
-		error_quark = g_quark_from_static_string ("openconnect-plugin-ui-error-quark");
-
-	return error_quark;
-}
-
-/* This should really be standard. */
-#define ENUM_ENTRY(NAME, DESC) { NAME, "" #NAME "", DESC }
-
-GType
-openconnect_plugin_ui_error_get_type (void)
-{
-	static GType etype = 0;
-
-	if (etype == 0) {
-		static const GEnumValue values[] = {
-			/* Unknown error. */
-			ENUM_ENTRY (OPENCONNECT_PLUGIN_UI_ERROR_UNKNOWN, "UnknownError"),
-			/* The specified property was invalid. */
-			ENUM_ENTRY (OPENCONNECT_PLUGIN_UI_ERROR_INVALID_PROPERTY, "InvalidProperty"),
-			/* The specified property was missing and is required. */
-			ENUM_ENTRY (OPENCONNECT_PLUGIN_UI_ERROR_MISSING_PROPERTY, "MissingProperty"),
-			{ 0, 0, 0 }
-		};
-		etype = g_enum_register_static ("OpenconnectPluginUiError", values);
-	}
-	return etype;
 }
 
 static gboolean

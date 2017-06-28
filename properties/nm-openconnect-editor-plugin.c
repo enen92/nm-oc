dlenski/network-manager-openconnect

commit 1ed6de801a398887277fbb9c35d9f5f5d6c4d5c3

Use new openconnect_{get,free}_supported_protocols in API version 5.5, instead of static list

properties/nm-openconnect-editor-plugin.c
@@ -360,15 +360,29 @@ notify_plugin_info_set (NMVpnEditorPlugin *plugin,
                         NMVpnPluginInfo *plugin_info)
 {
 	OpenconnectEditorPluginPrivate *priv = OPENCONNECT_EDITOR_PLUGIN_GET_PRIVATE (plugin);
-	const char *supported_protocols;
 	guint i, j;
 
 	if (!plugin_info)
 		return;
 
-	supported_protocols = nm_vpn_plugin_info_lookup_property (plugin_info, "openconnect", "supported-protocols");
-
 	g_strfreev (priv->supported_protocols);
+
+#if OPENCONNECT_CHECK_VER(5,5)
+	struct oc_vpn_proto *protos, *p;
+
+	if ((i = openconnect_get_supported_protocols(&protos)) < 0)
+		priv->supported_protocols = g_new0(gchar *, 1);
+	else {
+		priv->supported_protocols = g_new0(gchar *, i+1);
+		for (i=0, p=protos; p && p->name; p++) {
+			priv->supported_protocols[i] = g_strdup(p->name);
+		}
+		openconnect_free_supported_protocols(protos);
+	}
+
+#else
+	const char *supported_protocols = nm_vpn_plugin_info_lookup_property (plugin_info, "openconnect", "supported-protocols");
+
 	priv->supported_protocols = supported_protocols
 	    ? g_strsplit_set (supported_protocols, ",", -1)
 	    : g_new0 (char *, 1);
@@ -382,6 +396,7 @@ notify_plugin_info_set (NMVpnEditorPlugin *plugin,
 			priv->supported_protocols[i++] = priv->supported_protocols[j];
 	}
 	priv->supported_protocols[i] = NULL;
+#endif
 }
 
 static char **

auth-dialog/main.c
@@ -1073,12 +1073,12 @@ static int get_config (auth_ui_data *ui_data,
 	}
 
 	protocol = g_hash_table_lookup (options, NM_OPENCONNECT_KEY_PROTOCOL);
-	if (protocol && strcmp(protocol, "anyconnect")) {
 #if OPENCONNECT_CHECK_VER(5,1)
-		if (openconnect_set_protocol(vpninfo, protocol))
+	if (openconnect_set_protocol(vpninfo, protocol))
+#else
+	if (protocol && strcmp(protocol, "anyconnect"))
 #endif
-			return -EINVAL;
-	}
+		return -EINVAL;
 
 	cafile = g_hash_table_lookup (options, NM_OPENCONNECT_KEY_CACERT);
 	if (cafile)

properties/nm-openconnect-dialog.ui
@@ -872,11 +872,5 @@
           <!-- column-name protocol_value -->
           <column type="gchararray"/>
         </columns>
-        <data>
-          <row>
-            <col id="0" translatable="yes">Cisco AnyConnect</col>
-            <col id="1" translatable="no">anyconnect</col>
-          </row>
-        </data>
       </object>
 </interface>

properties/nm-openconnect-editor.c
@@ -217,19 +217,36 @@ init_token_ui (OpenconnectEditor *self,
 static gboolean
 init_protocol_combo_options (GtkComboBox *protocol_combo)
 {
-#if OPENCONNECT_CHECK_VER(5,1)
 	GtkListStore *protocol_combo_list = GTK_LIST_STORE (gtk_combo_box_get_model (protocol_combo));
 	GtkTreeIter iter;
 
+#if OPENCONNECT_CHECK_VER(5,5)
+	struct oc_vpn_proto *protos, *p;
+	openconnect_get_supported_protocols(&protos);
+	for (p=protos; p && p->name; p++) {
+		gtk_list_store_append(protocol_combo_list, &iter);
+		gtk_list_store_set(protocol_combo_list, &iter,
+						   0, p->pretty_name,
+						   1, p->name,
+						   -1);
+	}
+	openconnect_free_supported_protocols(protos);
+#else
+	gtk_list_store_append(protocol_combo_list, &iter);
+	gtk_list_store_set(protocol_combo_list, &iter,
+					   0, _("Cisco AnyConnect"),
+					   1, "anyconnect",
+					   -1);
+#  if OPENCONNECT_CHECK_VER(5,1)
 	gtk_list_store_append(protocol_combo_list, &iter);
 	gtk_list_store_set(protocol_combo_list, &iter,
 					   0, _("Juniper/Pulse Network Connect"),
 					   1, "nc",
 					   -1);
-	return TRUE;
-#else
-	return FALSE;
+#  endif
 #endif
+
+	return OPENCONNECT_CHECK_VER(5,1);
 }
 
 static gboolean

enen92/nm-oc

commit 6aa7d39d5f40957e3a4fa4d19f7ed434ee1bc5d2

properties: fix compiler warning about mixing declarations and code
  nm-openconnect-editor-plugin.c:396:14: error: ISO C90 forbids mixing declarations and code [-Werror,-Wdeclaration-after-statement]
          const char *supported_protocols = nm_vpn_plugin_info_lookup_property (plugin_info, "openconnect", "supported-protocols");

Fixes: 1ed6de801a398887277fbb9c35d9f5f5d6c4d5c3

properties/nm-openconnect-editor-plugin.c
@@ -367,36 +367,37 @@ notify_plugin_info_set (NMVpnEditorPlugin *plugin,
 
 	g_strfreev (priv->supported_protocols);
 
+	{
 #if OPENCONNECT_CHECK_VER(5,5)
-	struct oc_vpn_proto *protos, *p;
+		struct oc_vpn_proto *protos, *p;
 
-	if ((i = openconnect_get_supported_protocols(&protos)) < 0)
-		priv->supported_protocols = g_new0(gchar *, 1);
-	else {
-		priv->supported_protocols = g_new0(gchar *, i+1);
-		for (i=0, p=protos; p && p->name; p++) {
-			priv->supported_protocols[i] = g_strdup(p->name);
+		if ((i = openconnect_get_supported_protocols(&protos)) < 0)
+			priv->supported_protocols = g_new0(gchar *, 1);
+		else {
+			priv->supported_protocols = g_new0(gchar *, i+1);
+			for (i=0, p=protos; p && p->name; p++) {
+				priv->supported_protocols[i] = g_strdup(p->name);
+			}
+			openconnect_free_supported_protocols(protos);
 		}
-		openconnect_free_supported_protocols(protos);
-	}
-
 #else
-	const char *supported_protocols = nm_vpn_plugin_info_lookup_property (plugin_info, "openconnect", "supported-protocols");
+		const char *supported_protocols = nm_vpn_plugin_info_lookup_property (plugin_info, "openconnect", "supported-protocols");
 
-	priv->supported_protocols = supported_protocols
-	    ? g_strsplit_set (supported_protocols, ",", -1)
-	    : g_new0 (char *, 1);
+		priv->supported_protocols = supported_protocols
+		    ? g_strsplit_set (supported_protocols, ",", -1)
+		    : g_new0 (char *, 1);
 
-	/*remove empty entries and whitespace */
-	for (i = 0, j = 0; priv->supported_protocols[j]; j++) {
-		g_strstrip (priv->supported_protocols[j]);
-		if (priv->supported_protocols[j][0] == '\0')
-			g_free (priv->supported_protocols[j]);
-		else
-			priv->supported_protocols[i++] = priv->supported_protocols[j];
-	}
-	priv->supported_protocols[i] = NULL;
+		/*remove empty entries and whitespace */
+		for (i = 0, j = 0; priv->supported_protocols[j]; j++) {
+			g_strstrip (priv->supported_protocols[j]);
+			if (priv->supported_protocols[j][0] == '\0')
+				g_free (priv->supported_protocols[j]);
+			else
+				priv->supported_protocols[i++] = priv->supported_protocols[j];
+		}
+		priv->supported_protocols[i] = NULL;
 #endif
+	}
 }
 
 static char **